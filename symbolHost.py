776929dcb034564a79740b38b2eb9378c78a1c3b
TuggyBoat <tuggyboat@users.noreply.github.com>
2016-11-25 02:07:38 -0800

    Updated to include fallback in lookup

--- symbolHost.py
+++ symbolHost.py
@@ -29,13 +29,22 @@ def symbolQuery():
         if row == None:
             query = "SELECT * FROM Companies WHERE Name like \"%" + userquery + "%\""
         else:
+            curr.close()
             return {'status':'found', 'symbol': str(row[0]), 'name': str(row[1]), 'market': str(row[2])}
         curr.execute(query)
         row = curr.fetchone()
-        curr.close()
         if row == None:
-            return {'status':'notfound'}
+            userquery = userquery.split()[0]
+            query = "SELECT * FROM Companies WHERE Name like \"%" + userquery + "%\""
+            curr.execute(query)
+            row2 = curr.fetchone()
+            curr.close()
+            if row2 == None:
+                return {'status':'notfound'}
+            else:
+                return {'status':'found', 'symbol': str(row2[0]), 'name': str(row2[1]), 'market': str(row2[2])}
         else:
+            curr.close()
             return {'status':'found', 'symbol': str(row[0]), 'name': str(row[1]), 'market': str(row[2])}
 
     return {'status':'notfound'}
@@ -46,7 +55,7 @@ run(host='symbolLookup-boat.rhcloud.com', port=208, debug=True)
 
 
 
-
+        
 
 
 
