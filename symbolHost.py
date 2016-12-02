64e686192aa95b5513d53ccb3e9acc201ff50f0e
TuggyBoat <tuggyboat@users.noreply.github.com>
2016-12-01 18:58:18 -0800

    Updated to do lookups using possible spoken names

--- symbolHost.py
+++ symbolHost.py
@@ -27,25 +27,39 @@ def symbolQuery():
         curr.execute(query)
         row = curr.fetchone()
         if row == None:
-            query = "SELECT * FROM Companies WHERE Name like \"%" + userquery + "%\""
+            query = "SELECT * FROM Companies WHERE SName like \"%" + userquery + "%\""
         else:
             curr.close()
             return {'status':'found', 'symbol': str(row[0]), 'name': str(row[1]), 'market': str(row[2])}
         curr.execute(query)
-        row = curr.fetchone()
-        if row == None:
+        rows = curr.fetchall()
+        if rows == []:
             userquery = userquery.split()[0]
-            query = "SELECT * FROM Companies WHERE Name like \"%" + userquery + "%\""
+            query = "SELECT * FROM Companies WHERE SName like \"%" + userquery + "%\""
             curr.execute(query)
-            row2 = curr.fetchone()
+            rows2 = curr.fetchall()
             curr.close()
-            if row2 == None:
+            if rows2 == []:
                 return {'status':'notfound'}
             else:
-                return {'status':'found', 'symbol': str(row2[0]), 'name': str(row2[1]), 'market': str(row2[2])}
+                choices = []
+                hashedChoices = {}
+                for row in rows2:
+                    hashedChoices[row[1]] = row
+                    choices.append(row[1])
+                chosen = hashedChoices[process.extractOne(userquery, choices, scorer=fuzz.token_sort_ratio)[0]]
+                symbol = chosen[0].split()[0]
+                return {'status':'found', 'symbol': str(symbol), 'name': str(chosen[1]), 'market': str(chosen[2])}
         else:
             curr.close()
-            return {'status':'found', 'symbol': str(row[0]), 'name': str(row[1]), 'market': str(row[2])}
+            choices = []
+            hashedChoices = {}
+            for row in rows:
+                hashedChoices[row[1]] = row
+                choices.append(row[1])
+            chosen = hashedChoices[process.extractOne(userquery, choices, scorer=fuzz.token_sort_ratio)[0]]
+            symbol = chosen[0].split()[0]
+            return {'status':'found', 'symbol': str(symbol), 'name': str(chosen[1]), 'market': str(chosen[2])}
 
     return {'status':'notfound'}
 
